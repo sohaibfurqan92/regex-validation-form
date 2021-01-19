84bc2a66befedb8dc1a73454cdd28fdd4bf0d7bb
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-01-19 12:20:46 +0500

    Added validation for USA Zipcode format

--- app.js
+++ app.js
@@ -21,7 +21,17 @@ function validateEmail(){
 }
 
 function validateZip(){
+  const zip = document.getElementById('zip');
+  
+  //five digit zip code with and optional dash and 4 digits e.g. 55555-4444 or 55555
+  let re = /^\d{5}(-\d{4})?$/; //OR re=/^[0-9]{5}(-[0-9]{4})?$/;
 
+  if(!re.test(zip.value)){
+    zip.classList.add('is-invalid');
+  } else{
+    zip.classList.remove('is-invalid');
+
+  }
 }
 
 function validatePhone(){
